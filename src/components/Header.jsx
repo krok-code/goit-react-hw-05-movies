95cc23289857014680101f03b8526d2a8e707513
krok-code <maks.smolinskyi@gmail.com>
2024-01-27 22:59:18 +0200

    Add markup in MovieDitails component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 function Header() {
   return (
@@ -9,13 +9,13 @@ function Header() {
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Cinema</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link>
-              <Link to="/">Home</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="movies">Movies</Link>
-            </Nav.Link>
+          <Nav className="me-auto" variant="underline">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="movies" className="nav-link">
+              Movies
+            </NavLink>
           </Nav>
         </Container>
       </Navbar>
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,8 +1,84 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Card, Col, Image, Nav, Row } from 'react-bootstrap';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 
 const MovieDetails = () => {
-  return <Link to="/">Go back</Link>;
+  return (
+    <>
+      <Link to="/">
+        <div className="py-3">
+          {''}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentlColor"
+            className="bi bi-arrow-left"
+            viewBox=" 0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+            />
+          </svg>
+          {''}
+          Go back{''}
+        </div>
+      </Link>
+      <Card className="mb-3">
+        <Row className="g-0">
+          <Col md={4}>
+            <Image src="..." className="rounded-start" alt="..." fluid />
+          </Col>
+          <Col md={8}>
+            <Card.Body>
+              <Card.Title className="fs-2">Movie title</Card.Title>
+              <Card.Text>User score: 71%</Card.Text>
+              <Card.Text className="fs-5 fw-semibold">Overview</Card.Text>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <Card.Text className="fs-5 fw-semibold">Genres</Card.Text>
+              <Card.Text>Adventure Animation</Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+        <Row className="g-0">
+          <Col className="p-3">
+            <Card.Text className="fs-5 fw-semibold">
+              Additional Information
+            </Card.Text>
+            <Nav>
+              <Nav.Item>
+                <NavLink
+                  to="cast"
+                  className="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover p-2"
+                >
+                  Cast
+                </NavLink>
+              </Nav.Item>
+              <Nav.Item>
+                <NavLink
+                  to="reviews"
+                  className="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover p-2"
+                >
+                  Reviews
+                </NavLink>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+        <Row className="g-0">
+          <Col className="p-3">
+            {' '}
+            <Outlet />
+          </Col>
+        </Row>
+      </Card>
+    </>
+  );
 };
 
 export default MovieDetails;
