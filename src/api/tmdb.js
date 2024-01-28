9133e93f0efb0c70fb62edb87756e4cbd65c4f6b
krok-code <maks.smolinskyi@gmail.com>
2024-01-28 16:10:29 +0200

    Takes out constants

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -1,35 +1,48 @@
 import axios from 'axios';
+import {
+  INIT_FETCH_CONFIG,
+  DEFAULT_QUERY_PARAMS_FOR_SEARCH,
+} from 'utils/constant';
 
-const BASEURL = 'https://api.themoviedb.org/3';
+const tmdb = axios.create(INIT_FETCH_CONFIG);
+let apiConfigDetails = null;
 
-const CONFIG = {
-  method: 'GET',
-  BASEURL,
-  headers: {
-    accept: 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MGFhNzY1ZjlhMzVhOTdiNTBiZjI5NmNkZGIzZTBhNyIsInN1YiI6IjY0NTE4NGUwMzNhZDhmMDBmZWYyN2FhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ryw4dh3H0xPOea1Y2cbKZvI8bFHdhr2M3oPtuPEBQes',
-  },
-};
+async function getApiConfigDetails() {
+  const url = '/configuration';
+  const response = await tmdb(url);
+  return response.data;
+}
 
-const DEFAULT_QUERY_PARAMS_FOR_SEARCH = {
-  include_adult: false,
-  language: 'en-US',
-  page: 1,
-};
+getApiConfigDetails()
+  .then(config => (apiConfigDetails = config))
+  .catch(error =>
+    console.error(`Error in getting of API configuration:${error}`)
+  );
 
-const tmdb = axios.create(CONFIG);
+export function generateImageUrl(path, size) {
+  const baseUrl = apiConfigDetails.images.secure_base_url;
+  const url = baseUrl + size + path;
+  console.log(`Image url: `, url);
+  return url;
+}
 
 export async function getTrending(timeWindow = 'day', language = 'en-US') {
   const url = `/trending/movie/${timeWindow}`;
   const params = { language };
-  const response = await tmdb.get(url, { params });
+  const response = await tmdb(url, { params });
   return response.data.results;
 }
 
 export async function getMovieBySearch(query, page) {
   const url = `/search/movie`;
   const params = { ...DEFAULT_QUERY_PARAMS_FOR_SEARCH, query, page };
-  const response = await tmdb.get(url, { params });
+  const response = await tmdb(url, { params });
   return response.data.results;
+}
+
+export async function getMovieDetailsById(movieId, language = 'en-US') {
+  const url = `/movie/${movieId}`;
+  const params = { language };
+  const response = await tmdb.get(url, { params });
+  return response.data;
 }
---
+++ src/utils/constant.js
@@ -0,0 +1,24 @@
+const BASEURL = 'https://api.themoviedb.org/3';
+
+export const INIT_FETCH_CONFIG = {
+  method: 'GET',
+  baseURL: BASEURL,
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MGFhNzY1ZjlhMzVhOTdiNTBiZjI5NmNkZGIzZTBhNyIsInN1YiI6IjY0NTE4NGUwMzNhZDhmMDBmZWYyN2FhNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ryw4dh3H0xPOea1Y2cbKZvI8bFHdhr2M3oPtuPEBQes',
+  },
+};
+
+export const DEFAULT_QUERY_PARAMS_FOR_SEARCH = {
+  include_adult: false,
+  language: 'en-US',
+  page: 1,
+};
+
+export const states = {
+  IDLE: 'IDLE',
+  LOADING: 'LOADING',
+  LOADED: 'LOADED',
+  ERROR: 'ERROR',
+};
