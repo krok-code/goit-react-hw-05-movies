6b3fa92cff4db1279866567c05056a063b9d7cd7
krok-code <maks.smolinskyi@gmail.com>
2024-01-29 00:38:00 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,33 +2,41 @@ import { useEffect, useState } from 'react';
 import { getTrending } from 'api/tmdb';
 import MoviesList from 'components/MoviesList';
 import CenteredSpinner from 'components/CenteredSpinner';
+import { states } from 'utils/constant';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, setState] = useState(states.LOADING);
 
   useEffect(() => {
     getTrending()
       .then(data => {
         setMovies(data);
-        setLoading(false);
+        setState(states.LOADED);
       })
       .catch(error => {
-        setError(error);
-        setLoading(false);
+        setState(states.ERROR);
       });
   }, []);
 
-  if (loading) return <CenteredSpinner />;
-  if (error) return <p>Oops, something went wrong. Please try again later.</p>;
+  switch (state) {
+    case states.LOADING:
+      return <CenteredSpinner />;
 
-  return (
-    <>
-      <h1 className="nb-3">Trending today</h1>
-      <MoviesList movies={movies} />
-    </>
-  );
+    case states.LOADED:
+      return (
+        <>
+          <h1 className="nb-3">Trending today</h1>
+          <MoviesList movies={movies} />
+        </>
+      );
+
+    case states.ERROR:
+      return <p>Oops, something went wrong. Please try again later.</p>;
+
+    default:
+      return null;
+  }
 };
 
 export default Home;
