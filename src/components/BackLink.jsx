8bc581bfd9208a674e5200645339dd2a1230e533
krok-code <maks.smolinskyi@gmail.com>
2024-02-05 21:22:06 +0200

    Update BackLink.jsx

--- src/components/BackLink.jsx
+++ src/components/BackLink.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { ReactComponent as ArrowLeft } from '../images/left-arrow.svg';
 
-const BackLink = ({ from }) => {
+const BackLink = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <Link to={from}>
-      <div className="py-3">
-        {' '}
-        <ArrowLeft />
-        Go back{' '}
-      </div>
-    </Link>
+    <div className="py-3" onClick={goBack} style={{ cursor: 'pointer' }}>
+      {' '}
+      <ArrowLeft />
+      Go back{' '}
+    </div>
   );
 };
 
